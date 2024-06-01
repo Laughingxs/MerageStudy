e81528236e6255d12c99bfba04dd5830dc4f9984
root <910423947@qq.com>
2024-06-01 22:52:39 +0800

    hello7

--- hello1.cpp
+++ hello1.cpp
@@ -4,5 +4,12 @@
 void hello1() {
     std::cout << "Hello from hello1!" << std::endl;
     std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
+    std::cout << "This is a test!" << std::endl;
     // This line is intentionally left blank
 }
